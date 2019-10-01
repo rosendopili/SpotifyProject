53852ddd05c9849d27176fd5ee000986a2afa003
rosendopili <rosendopili@gmail.com>
2019-09-30 21:24:14 -0400

    looks like the build is compiling with no errors.  Moving on to postman testing

--- spring-boot-monolith/src/main/java/com/example/springbootmonolith/service/UserRoleServiceImpl.java
+++ spring-boot-monolith/src/main/java/com/example/springbootmonolith/service/UserRoleServiceImpl.java
@@ -3,13 +3,12 @@ package com.example.springbootmonolith.service;
 import com.example.springbootmonolith.models.UserRole;
 import com.example.springbootmonolith.repository.UserRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Repository
 @Service
 public class UserRoleServiceImpl implements UserRoleService {
 
+    @Autowired
     UserRoleRepository userRoleRepository;
 
     @Override
